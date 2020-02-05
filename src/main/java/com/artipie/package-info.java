adf30a3aa010d8e9f590c3b2e208c1841d9ceaf7
Kirill <g4s8.public@gmail.com>
2020-02-05 14:05:11 +0300

    #12 - using artipie parent pom

--- src/main/java/com/artipie/package-info.java
+++ src/main/java/com/artipie/package-info.java
@@ -1,7 +1,7 @@
-/**
- * The MIT License (MIT)
+/*
+ * MIT License
  *
- * Copyright (c) 2020 Yegor Bugayenko
+ * Copyright (c) 2020 Artipie
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -10,36 +10,22 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.yegor256.artipie;
-
-import org.junit.Test;
 
 /**
- * Test case for {@link Main}.
+ * Artipie files.
  *
- * @author Yegor Bugayenko (yegor256@gmail.com)
- * @version $Id$
  * @since 0.1
  */
-public final class MainTest {
+package com.artipie;
 
-    /**
-     * Runs the app.
-     * @throws Exception If some problem inside
-     */
-    @Test
-    public void runsTheApp() throws Exception {
-        Main.main("hello", "world");
-    }
-}
--- white-paper/README.md
+++
@@ -1,7 +0,0 @@
-Just run this in order to build it into PDF:
-
-```bash
-$ latexmk -pdf wp
-```
-
-You will need to have LaTeX installed, of course.
