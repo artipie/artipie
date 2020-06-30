4f1c2f4667a728779c6f40d16c822b0672dbb3fe
paulodamaso <pauloeduardolobo@gmail.com>
2020-06-30 16:35:42 -0300

    For #203: allowing the use of two configuration schemas

--- src/main/java/com/artipie/auth/AuthFromYaml.java
+++ src/main/java/com/artipie/auth/AuthFromYaml.java
@@ -26,6 +26,8 @@ package com.artipie.auth;
 import com.amihaiemil.eoyaml.YamlMapping;
 import com.artipie.http.auth.Authentication;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.codec.digest.DigestUtils;
 
 /**
@@ -38,7 +40,16 @@ import org.apache.commons.codec.digest.DigestUtils;
  *  This configuration would mean that Joe should use his email to login instead of username. Do not
  *  forget to validate credentials, logins should be unique.
  */
+@SuppressWarnings({
+    "PMD.AvoidDeeplyNestedIfStmts",
+    "PMD.AvoidDuplicateLiterals",
+    "PMD.ConfusingTernary"})
 public final class AuthFromYaml implements Authentication {
+
+    /**
+     * Password format.
+     */
+    private static final Pattern PSWD_FORMAT = Pattern.compile("(plain:|sha256:)(.+)");
 
     /**
      * YAML credentials settings.
@@ -57,18 +68,27 @@ public final class AuthFromYaml implements Authentication {
     public Optional<String> user(final String user, final String pass) {
         final YamlMapping users = this.cred.yamlMapping("credentials");
         Optional<String> res = Optional.empty();
+        //@checkstyle NestedIfDepthCheck (10 lines)
         if (users != null && users.yamlMapping(user) != null) {
             final String stored = users.yamlMapping(user).string("pass");
-            final String type = users.yamlMapping(user).string("type");
-            if (stored != null && type != null && check(stored, type, pass)) {
-                res = Optional.of(user);
+            if (stored != null) {
+                final String type = users.yamlMapping(user).string("type");
+                if (type != null) {
+                    if (check(stored, type, pass)) {
+                        res = Optional.of(user);
+                    }
+                } else {
+                    if (check(stored, pass)) {
+                        res = Optional.of(user);
+                    }
+                }
             }
         }
         return res;
     }
 
     /**
-     * Checks stored password against the given one.
+     * Checks stored password against the given one with type.
      * @param stored Password from settings
      * @param type Type of Password from settings
      * @param given Password to check
@@ -79,4 +99,20 @@ public final class AuthFromYaml implements Authentication {
             || given.equals(stored);
     }
 
+    /**
+     * Checks stored password against the given one.
+     * @param stored Password from settings
+     * @param given Password to check
+     * @return True if passwords are the same
+     */
+    private static boolean check(final String stored, final String given) {
+        boolean res = false;
+        final Matcher matcher = AuthFromYaml.PSWD_FORMAT.matcher(stored);
+        if (matcher.matches()) {
+            final String pswd = matcher.group(2);
+            res = stored.startsWith("sha256") && DigestUtils.sha256Hex(given).equals(pswd)
+                || given.equals(pswd);
+        }
+        return res;
+    }
 }
--- src/test/java/com/artipie/auth/AuthFromYamlTest.java
+++ src/test/java/com/artipie/auth/AuthFromYamlTest.java
@@ -35,7 +35,10 @@ import org.junit.jupiter.api.Test;
  * Test for {@link AuthFromYaml}.
  * @since 0.3
  */
-@SuppressWarnings({"PMD.AvoidDuplicateLiterals", "PMD.UseObjectForClearerAPI"})
+@SuppressWarnings({
+    "PMD.AvoidDuplicateLiterals",
+    "PMD.TooManyMethods",
+    "PMD.UseObjectForClearerAPI"})
 final class AuthFromYamlTest {
 
     @Test
@@ -44,11 +47,7 @@ final class AuthFromYamlTest {
         final String pass = "qwerty";
         MatcherAssert.assertThat(
             new AuthFromYaml(
-                AuthFromYamlTest.settings(
-                    user,
-                    "plain",
-                    pass
-                )
+                AuthFromYamlTest.settings(user, String.format("plain:%s", pass))
             ).user(user, pass).get(),
             new IsEqual<>(user)
         );
@@ -61,9 +60,7 @@ final class AuthFromYamlTest {
         MatcherAssert.assertThat(
             new AuthFromYaml(
                 AuthFromYamlTest.settings(
-                    user,
-                    "sha256",
-                    DigestUtils.sha256Hex(pass)
+                    user, String.format("sha256:%s", DigestUtils.sha256Hex(pass))
                 )
             ).user(user, pass).get(),
             new IsEqual<>(user)
@@ -75,11 +72,7 @@ final class AuthFromYamlTest {
         final String user = "mark";
         MatcherAssert.assertThat(
             new AuthFromYaml(
-                AuthFromYamlTest.settings(
-                    user,
-                    "plain",
-                    "123"
-                )
+                AuthFromYamlTest.settings(user, "plain:123")
             ).user(user, "456").isEmpty(),
             new IsEqual<>(true)
         );
@@ -91,11 +84,21 @@ final class AuthFromYamlTest {
         MatcherAssert.assertThat(
             new AuthFromYaml(
                 AuthFromYamlTest.settings(
-                    "ann",
-                    "sha256",
-                    DigestUtils.sha256Hex(pass)
+                    "ann", String.format("sha256:%s", DigestUtils.sha256Hex(pass))
                 )
             ).user("anna", pass).isEmpty(),
+            new IsEqual<>(true)
+        );
+    }
+
+    @Test
+    void doesNotAuthoriseWhenPassIsMalformed() {
+        final String pass = "098";
+        final String user = "barton";
+        MatcherAssert.assertThat(
+            new AuthFromYaml(
+                AuthFromYamlTest.settings(user, "098")
+            ).user(user, pass).isEmpty(),
             new IsEqual<>(true)
         );
     }
@@ -109,10 +112,7 @@ final class AuthFromYamlTest {
         MatcherAssert.assertThat(
             new AuthFromYaml(
                 AuthFromYamlTest.settings(
-                    user,
-                    "sha256",
-                    DigestUtils.sha256Hex(pass),
-                    login
+                    user, String.format("sha256:%s", DigestUtils.sha256Hex(pass)), login
                 )
             ).user(login, pass).get(),
             new IsEqual<>(user)
@@ -127,6 +127,104 @@ final class AuthFromYamlTest {
         MatcherAssert.assertThat(
             new AuthFromYaml(
                 AuthFromYamlTest.settings(
+                    user,
+                    String.format("sha256:%s", DigestUtils.sha256Hex(pass)),
+                    "sasha@example.com"
+                )
+            ).user(user, pass).isEmpty(),
+            new IsEqual<>(true)
+        );
+    }
+
+    @Test
+    void authorisesByPlainPasswordWithSimplerSettings() {
+        final String user = "john";
+        final String pass = "qwerty";
+        MatcherAssert.assertThat(
+            new AuthFromYaml(
+                AuthFromYamlTest.simpleSettings(
+                    user,
+                    "plain",
+                    pass
+                )
+            ).user(user, pass).get(),
+            new IsEqual<>(user)
+        );
+    }
+
+    @Test
+    void authorisesByHashedPasswordWithSimplerSettings() {
+        final String user = "bob";
+        final String pass = "123";
+        MatcherAssert.assertThat(
+            new AuthFromYaml(
+                AuthFromYamlTest.simpleSettings(
+                    user,
+                    "sha256",
+                    DigestUtils.sha256Hex(pass)
+                )
+            ).user(user, pass).get(),
+            new IsEqual<>(user)
+        );
+    }
+
+    @Test
+    void doesNotAuthoriseByWrongPasswordWithSimplerSettings() {
+        final String user = "mark";
+        MatcherAssert.assertThat(
+            new AuthFromYaml(
+                AuthFromYamlTest.simpleSettings(
+                    user,
+                    "plain",
+                    "123"
+                )
+            ).user(user, "456").isEmpty(),
+            new IsEqual<>(true)
+        );
+    }
+
+    @Test
+    void doesNotAuthoriseByWrongLoginWithSimplerSettings() {
+        final String pass = "abc";
+        MatcherAssert.assertThat(
+            new AuthFromYaml(
+                AuthFromYamlTest.simpleSettings(
+                    "ann",
+                    "sha256",
+                    DigestUtils.sha256Hex(pass)
+                )
+            ).user("anna", pass).isEmpty(),
+            new IsEqual<>(true)
+        );
+    }
+
+    @Test
+    @Disabled
+    void authorisesByAlternativeLoginWithSimplerSettings() {
+        final String user = "mary";
+        final String pass = "def";
+        final String login = "mary@example.com";
+        MatcherAssert.assertThat(
+            new AuthFromYaml(
+                AuthFromYamlTest.simpleSettings(
+                    user,
+                    "sha256",
+                    DigestUtils.sha256Hex(pass),
+                    login
+                )
+            ).user(login, pass).get(),
+            new IsEqual<>(user)
+        );
+    }
+
+    @Test
+    @Disabled
+    void doesNotAuthoriseByMainLoginWithSimplerSettings() {
+        final String user = "sasha";
+        final String pass = "999";
+        MatcherAssert.assertThat(
+            new AuthFromYaml(
+                AuthFromYamlTest.simpleSettings(
                     user,
                     "sha256",
                     DigestUtils.sha256Hex(pass),
@@ -144,7 +242,7 @@ final class AuthFromYamlTest {
      * @param pass Password
      * @return Settings
      */
-    private static YamlMapping settings(
+    private static YamlMapping simpleSettings(
         final String user,
         final String type,
         final String pass) {
@@ -169,8 +267,8 @@ final class AuthFromYamlTest {
      * @return Settings
      * @checkstyle ParameterNumberCheck (3 lines)
      */
-    private static YamlMapping settings(final String user, final String type, final String pass,
-        final String login) {
+    private static YamlMapping simpleSettings(final String user, final String type,
+        final String pass, final String login) {
         return Yaml.createYamlMappingBuilder().add(
             "credentials",
             Yaml.createYamlMappingBuilder()
@@ -180,6 +278,39 @@ final class AuthFromYamlTest {
                     .add("type", type)
                     .add("pass", pass)
                     .add("login", login).build()
+                ).build()
+        ).build();
+    }
+
+    /**
+     * Composes yaml settings.
+     * @param user User
+     * @param pass Password
+     * @return Settings
+     */
+    private static YamlMapping settings(final String user, final String pass) {
+        return Yaml.createYamlMappingBuilder().add(
+            "credentials",
+            Yaml.createYamlMappingBuilder()
+                .add(user, Yaml.createYamlMappingBuilder().add("pass", pass).build())
+                .build()
+        ).build();
+    }
+
+    /**
+     * Composes yaml settings.
+     * @param user User
+     * @param pass Password
+     * @param login Alternative login
+     * @return Settings
+     */
+    private static YamlMapping settings(final String user, final String pass, final String login) {
+        return Yaml.createYamlMappingBuilder().add(
+            "credentials",
+            Yaml.createYamlMappingBuilder()
+                .add(
+                    user,
+                    Yaml.createYamlMappingBuilder().add("pass", pass).add("login", login).build()
                 ).build()
         ).build();
     }
