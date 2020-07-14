ad43dc00232899c4276c1a2f2b468e302cfddb6f
Sammers21 <titantins@gmail.com>
2020-07-14 12:10:22 +0300

    #154 - move bash into a script

---
+++ examples/binary/run.sh
@@ -0,0 +1,18 @@
+set -x
+set -e
+# start artipie
+docker run --name artipie -d -it -v $(pwd)/artipie.yaml:/etc/artipie.yml -v $(pwd):/var/artipie -p 8080:80 artipie/artipie:latest
+# wait for container to be ready for the new connections
+sleep 5
+# create a file for subsequent publication
+echo "hello world" > text.txt
+# publish text.txt
+curl --silent -X POST --data-binary "@text.txt" http://localhost:8080/repo/text.txt
+# download the file
+STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X GET http://localhost:8080/repo/text.txt)
+# remove container
+docker rm -f artipie
+# make sure status code is true
+if test $STATUSCODE -ne 200; then
+  exit 1
+fi
