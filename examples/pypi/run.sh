fde0ac0ab7b2df2da1b87ec4eefa3d2c5cca3d15
olenagerasimova <olena.gerasimova@gmail.com>
2020-07-30 21:09:56 +0300

    Python proof CR

--- examples/pypi/run.sh
+++ examples/pypi/run.sh
@@ -7,16 +7,16 @@ docker run --name artipie -d -it -v $(pwd)/artipie.yaml:/etc/artipie.yml -v $(pw
 # Wait for container to be ready for new connections.
 sleep 5
 
-# Build and upload python project to Artipie
+# Build and upload python project to artipie.
 cd sample-project
 python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 python3 -m pip install --user --upgrade twine
-python3 -m twine upload --repository-url http://localhost:8080/my-pypi -u username -p 123 dist/*
+python3 -m twine upload --repository-url http://localhost:8080/my-pypi -u username -p password dist/*
 cd ..
 
 # Install earlier uploaded python package from artipie.
-python3 -m pip install --index-url http://localhost:8080/my-pypi --no-deps sample_project
+python3 -m pip install --index-url http://localhost:8080/my-pypi sample_project
 
 # Remove container.
 docker stop artipie
