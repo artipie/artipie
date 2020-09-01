176a1237f6cc085c5f7c58a80752f66242376f02
Sammers21 <titantins@gmail.com>
2020-09-01 18:20:55 +0300

    #154 - 'my-go-proxy' -> 'my-go'

--- examples/go/run.sh
+++ examples/go/run.sh
@@ -9,7 +9,7 @@ sleep 5
 
 # Force go client to use Aritpie Go registry.
 export GO111MODULE=on
-export GOPROXY=http://localhost:8080/my-go-proxy
+export GOPROXY=http://localhost:8080/my-go
 
 # Install from Artipie Go registry.
 go get -x -insecure golang.org/x/time
