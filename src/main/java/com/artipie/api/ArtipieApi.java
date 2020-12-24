d5275bc98cba6052b7a2e9c30f98d8a9223e84c1
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2020-12-24 17:57:28 +0300

    #797 - Bump to 0.4.1

--- src/main/java/com/artipie/api/ArtipieApi.java
+++ src/main/java/com/artipie/api/ArtipieApi.java
@@ -23,19 +23,11 @@
  */
 package com.artipie.api;
 
-import com.amihaiemil.eoyaml.Scalar;
-import com.amihaiemil.eoyaml.Yaml;
 import com.amihaiemil.eoyaml.YamlMapping;
-import com.amihaiemil.eoyaml.YamlMappingBuilder;
 import com.artipie.RepoPermissionsFromStorage;
 import com.artipie.Settings;
 import com.artipie.YamlPermissions;
-import com.artipie.asto.Concatenation;
-import com.artipie.asto.Key;
-import com.artipie.asto.Remaining;
-import com.artipie.http.Response;
 import com.artipie.http.Slice;
-import com.artipie.http.async.AsyncResponse;
 import com.artipie.http.async.AsyncSlice;
 import com.artipie.http.headers.Header;
 import com.artipie.http.rq.RequestLineFrom;
@@ -47,38 +39,24 @@ import com.artipie.http.rt.ByMethodsRule;
 import com.artipie.http.rt.RtRule;
 import com.artipie.http.rt.RtRulePath;
 import com.artipie.http.rt.SliceRoute;
-import com.artipie.management.ConfigFiles;
 import com.artipie.management.api.ApiAuthSlice;
 import com.artipie.management.api.ApiChangeUserPassword;
+import com.artipie.management.api.ApiRepoGetSlice;
 import com.artipie.management.api.ApiRepoListSlice;
 import com.artipie.management.api.ApiRepoUpdateSlice;
 import com.artipie.management.api.ContentAsYaml;
 import com.artipie.management.api.CookiesAuthScheme;
-import com.artipie.management.api.RsYaml;
-import com.artipie.management.api.artifactory.AddUpdatePermissionSlice;
-import com.artipie.management.api.artifactory.AddUpdateUserSlice;
-import com.artipie.management.api.artifactory.CreateRepoSlice;
-import com.artipie.management.api.artifactory.DeletePermissionSlice;
-import com.artipie.management.api.artifactory.DeleteUserSlice;
-import com.artipie.management.api.artifactory.FromRqLine;
-import com.artipie.management.api.artifactory.GetPermissionSlice;
-import com.artipie.management.api.artifactory.GetPermissionsSlice;
-import com.artipie.management.api.artifactory.GetStorageSlice;
-import com.artipie.management.api.artifactory.GetUserSlice;
-import com.artipie.management.api.artifactory.GetUsersSlice;
+import com.artipie.management.api.artifactory.*;
 import com.artipie.repo.ArtipieStorages;
 import com.artipie.repo.ConfigFile;
 import com.artipie.repo.ConfigFileApi;
 import hu.akarnokd.rxjava2.interop.SingleInterop;
 import io.reactivex.Single;
-import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
-import org.reactivestreams.Publisher;
 
 /**
  * Artipie API endpoints.
@@ -282,65 +260,4 @@ public final class ArtipieApi extends Slice.Wrap {
         );
     }
 
-    /**
-     * Try to identify cause of hang of github actions. Should be removed.
-     * @since 0.14
-     */
-    static final class ApiRepoGetSlice implements Slice {
-
-        /**
-         * URI path pattern.
-         */
-        private static final Pattern PTN = Pattern.compile("/api/repos/(?<key>[^/.]+/[^/.]+)");
-
-        /**
-         * Config file to support `yaml` and `.yml` extensions.
-         */
-        private final ConfigFiles configfile;
-
-        /**
-         * New repo API.
-         * @param configfile Config file to support `yaml` and `.yml` extensions
-         */
-        ApiRepoGetSlice(final ConfigFiles configfile) {
-            this.configfile = configfile;
-        }
-
-        @Override
-        @SuppressWarnings("PMD.AvoidDuplicateLiterals")
-        public Response response(final String line, final Iterable<Map.Entry<String, String>> headers,
-            final Publisher<ByteBuffer> body) {
-            final Matcher matcher = PTN.matcher(new RequestLineFrom(line).uri().getPath());
-            if (!matcher.matches()) {
-                throw new IllegalStateException("Should match");
-            }
-            final String name = matcher.group("key");
-            final Key.From key = new Key.From(String.format("%s.yaml", name));
-            // @checkstyle LineLengthCheck (50 lines)
-            return new AsyncResponse(
-                SingleInterop.fromFuture(this.configfile.exists(key)).filter(exists -> exists)
-                    .flatMapSingleElement(
-                        ignore -> SingleInterop.fromFuture(this.configfile.value(key))
-                            .flatMap(pub -> new Concatenation(pub).single())
-                            .map(
-                                data -> Yaml.createYamlInput(
-                                    new String(new Remaining(data).bytes(), StandardCharsets.UTF_8)
-                                ).readYamlMapping()
-                            ).map(
-                                config -> {
-                                    final YamlMapping repo = config.yamlMapping("repo");
-                                    YamlMappingBuilder builder = Yaml.createYamlMappingBuilder();
-                                    builder = builder.add("type", repo.value("type"));
-                                    if (repo.value("storage") != null
-                                        && Scalar.class.isAssignableFrom(repo.value("storage").getClass())) {
-                                        builder = builder.add("storage", repo.value("storage"));
-                                    }
-                                    builder = builder.add("permissions", repo.value("permissions"));
-                                    return Yaml.createYamlMappingBuilder().add("repo", builder.build()).build();
-                                }
-                            ).<Response>map(RsYaml::new)
-                    ).switchIfEmpty(Single.just(new RsWithStatus(RsStatus.NOT_FOUND)))
-            );
-        }
-    }
 }
