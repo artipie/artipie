7e9247315d35e2dece7cea88ea0a7846a502b500
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2021-08-31 18:15:36 +0300

    feat: add TTL and max size for cache storages in proxy repos

---
+++ src/main/java/com/artipie/repo/YamlCacheStorage.java
@@ -0,0 +1,74 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/artipie/LICENSE.txt
+ */
+package com.artipie.repo;
+
+import com.amihaiemil.eoyaml.YamlMapping;
+import com.artipie.YamlStorage;
+import com.artipie.asto.Storage;
+import java.time.Duration;
+
+/**
+ * Proxy cache storage config from YAML.
+ * @since 0.23
+ */
+final class YamlCacheStorage implements CacheStorage {
+    /**
+     * Cache storage.
+     */
+    private final Storage cstorage;
+
+    /**
+     * Max available size.
+     */
+    private final Long cmaxsize;
+
+    /**
+     * Time to live.
+     */
+    private final Duration ttl;
+
+    /**
+     * Ctor.
+     * @param source Source YAML with only cache section
+     */
+    YamlCacheStorage(final YamlMapping source) {
+        this(new YamlStorage(source).storage());
+    }
+
+    /**
+     * Ctor with default max size and time to live.
+     * @param storage Cache storage
+     */
+    YamlCacheStorage(final Storage storage) {
+        this(storage, Long.MAX_VALUE, Duration.ofHours(Long.MAX_VALUE));
+    }
+
+    /**
+     * Ctor.
+     * @param cstorage Cache storage
+     * @param maxsize Max available size
+     * @param ttl Time to live
+     */
+    YamlCacheStorage(final Storage cstorage, final Long maxsize, final Duration ttl) {
+        this.cstorage = cstorage;
+        this.cmaxsize = maxsize;
+        this.ttl = ttl;
+    }
+
+    @Override
+    public Storage storage() {
+        return this.cstorage;
+    }
+
+    @Override
+    public Long maxSize() {
+        return this.cmaxsize;
+    }
+
+    @Override
+    public Duration timeToLive() {
+        return this.ttl;
+    }
+}
