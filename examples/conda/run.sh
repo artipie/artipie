dd187362327e8622f5f5f6a7b86af19525c25e4c
olenagerasimova <olena.gereasimova@gmail.com>
2021-09-06 16:10:04 +0300

    install corrected

--- examples/conda/run.sh
+++ examples/conda/run.sh
@@ -13,4 +13,4 @@ anaconda login --username any --password any
 conda build --output-folder ./conda-out/ ./example-package/conda/
 
 # Install the package.
-conda install -y example-project
+conda install -y example-package
