ae619000e303213e366453489460a87ec898fe54
olenagerasimova <olena.gereasimova@gmail.com>
2021-09-06 11:26:08 +0300

    Anaconda example

---
+++ examples/conda/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Login (required by anaconda client)
+anaconda login --username any --password any
+
+# Build and push package.
+conda build --output-folder ./conda-out/ ./example-project/conda/
+
+# Install the package.
+conda install -y example-project
