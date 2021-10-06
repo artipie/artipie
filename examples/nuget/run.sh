9787c4de478be5cfc070189f6814925be1c2e776
olenagerasimova <olena.gereasimova@gmail.com>
2021-10-06 13:09:07 +0300

    script corrected

--- examples/nuget/run.sh
+++ examples/nuget/run.sh
@@ -2,4 +2,4 @@
 
 nuget push ./7faad2d4-c3c2-4c23-a816-a09d1b1f89e8 -ConfigFile ./NuGet.Config -Verbosity detailed -Source artipie-nuget-test
 
-nuget install Newtonsoft.Json -Version 12.0.3 -NoCache -ConfigFile /home/NuGet.Config -Verbosity detailed -Source artipie-nuget-test
+nuget install Newtonsoft.Json -Version 12.0.3 -NoCache -ConfigFile ./NuGet.Config -Verbosity detailed -Source artipie-nuget-test
