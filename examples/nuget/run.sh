630cd5dcfde63a0c7d90f2558daaa235aaeafd02
olenagerasimova <olena.gereasimova@gmail.com>
2021-10-06 12:50:23 +0300

    add install

--- examples/nuget/run.sh
+++ examples/nuget/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 nuget push ./7faad2d4-c3c2-4c23-a816-a09d1b1f89e8 -ConfigFile ./NuGet.Config -Verbosity detailed -Source artipie-nuget-test
+
+nuget install Newtonsoft.Json -Version 12.0.3 -NoCache -ConfigFile /home/NuGet.Config -Verbosity detailed -Source artipie-nuget-test
