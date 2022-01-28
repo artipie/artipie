f46dbb576f7fefbe9354e158698a752fdeb66964
Olivier B. OURA <baudoliver7@gmail.com>
2022-01-28 15:09:46 +0000

    test(PromuSlice): use testcontainers for integration tests Ref: #980

--- src/test/java/com/artipie/PromuSliceITCase.java
+++ src/test/java/com/artipie/PromuSliceITCase.java
@@ -4,22 +4,14 @@
  */
 package com.artipie;
 
-import com.amihaiemil.eoyaml.Yaml;
-import com.amihaiemil.eoyaml.YamlMappingBuilder;
-import com.artipie.http.client.jetty.JettyClientSlices;
-import com.artipie.http.headers.Accept;
-import com.artipie.http.rs.RsStatus;
-import io.vertx.reactivex.core.Vertx;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import javax.ws.rs.HttpMethod;
-import javax.ws.rs.core.MediaType;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.core.IsEqual;
+import com.artipie.test.ContainerResultMatcher;
+import com.artipie.test.TestDeployment;
+import org.cactoos.map.MapEntry;
+import org.cactoos.map.MapOf;
+import org.hamcrest.core.StringContains;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 /**
  * IT tests for Prometheus metrics.
@@ -28,91 +20,58 @@ import org.junit.jupiter.api.io.TempDir;
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 final class PromuSliceITCase {
 
-    @Test
-    void givesAccessWhenPrometheusConfigIsAvailable(
-        @TempDir final Path root) throws Exception {
-        final int port = runServer(
-            root,
-            Yaml.createYamlMappingBuilder()
-                .add(
-                    "metrics",
-                    Yaml.createYamlMappingBuilder()
-                        .add("type", "prometheus")
-                        .build()
-                )
-        );
-        final HttpURLConnection con = (HttpURLConnection) new URL(formatUrl(port))
-            .openConnection();
-        con.setRequestMethod(HttpMethod.GET);
-        con.setRequestProperty(Accept.NAME, MediaType.TEXT_PLAIN);
-        MatcherAssert.assertThat(
-            con.getResponseCode(),
-            new IsEqual<>(
-                Integer.parseInt(RsStatus.OK.code())
-            )
-        );
-        con.disconnect();
-    }
-
-    @Test
-    void blocksAccessWhenPrometheusConfigIsUnavailable(
-        @TempDir final Path root) throws Exception {
-        final int port = runServer(
-            root,
-            Yaml.createYamlMappingBuilder()
-        );
-        final HttpURLConnection con = (HttpURLConnection) new URL(formatUrl(port))
-            .openConnection();
-        con.setRequestMethod(HttpMethod.GET);
-        con.setRequestProperty(Accept.NAME, MediaType.TEXT_PLAIN);
-        MatcherAssert.assertThat(
-            con.getResponseCode(),
-            new IsEqual<>(
-                Integer.parseInt(RsStatus.NOT_FOUND.code())
-            )
-        );
-        con.disconnect();
-    }
-
     /**
-     * Runs an Artipie server.
-     * @param root Server root folder
-     * @param meta Meta config
-     * @return Port on which the server starts
+     * Test deployments.
+     * @checkstyle VisibilityModifierCheck (10 lines)
      */
-    private static int runServer(
-        final Path root, final YamlMappingBuilder meta) throws Exception {
-        final Vertx vertx = Vertx.vertx();
-        final Path repos = root.resolve("repos");
-        repos.toFile().mkdir();
-        final Path cfg = root.resolve("artipie.yaml");
-        Files.write(
-            cfg,
-            Yaml.createYamlMappingBuilder()
-                .add(
-                    "meta",
-                    meta.add(
-                        "storage",
-                        Yaml.createYamlMappingBuilder()
-                            .add("type", "fs")
-                            .add("path", repos.toString())
-                            .build()
-                    ).build()
-                )
-                .build().toString().getBytes()
+    @RegisterExtension
+    final TestDeployment containers = new TestDeployment(
+        new MapOf<>(
+            new MapEntry<>(
+                "simple-artipie",
+                () -> TestDeployment.ArtipieContainer.defaultDefinition()
+            ),
+            new MapEntry<>(
+                "artipie-prometheus",
+                () -> TestDeployment.ArtipieContainer.defaultDefinition()
+                    .withConfig("artipie-metrics-prometheus.yaml")
+            )
+        ),
+        () -> new TestDeployment.ClientContainer("alpine:3.11")
+            .withWorkingDirectory("/w")
+    );
+
+    @BeforeEach
+    void setUp() throws Exception {
+        this.containers.assertExec(
+            "Failed to install deps",
+            new ContainerResultMatcher(),
+            "apk", "add", "--no-cache", "curl"
         );
-        final JettyClientSlices http = new JettyClientSlices(new HttpClientSettings());
-        http.start();
-        final VertxMain server = new VertxMain(http, cfg, vertx, 0);
-        return server.start();
     }
 
-    /**
-     * Gives Prometheus metrics url.
-     * @param port Port
-     * @return Prometheus metrics url
-     */
-    private static String formatUrl(final int port) {
-        return String.format("http://localhost:%s/prometheus/metrics", port);
+    @Test
+    void givesAccessWhenPrometheusConfigIsAvailable() throws Exception {
+        this.containers.assertExec(
+            "Failed to download artifact",
+            new ContainerResultMatcher(
+                ContainerResultMatcher.SUCCESS,
+                new StringContains("HTTP/1.1 200 OK")
+            ),
+            "curl", "-X", "GET", "http://artipie-prometheus:8080/prometheus/metrics"
+        );
     }
+
+    @Test
+    void blocksAccessWhenPrometheusConfigIsUnavailable() throws Exception {
+        this.containers.assertExec(
+            "Failed to download artifact",
+            new ContainerResultMatcher(
+                ContainerResultMatcher.SUCCESS,
+                new StringContains("HTTP/1.1 404 Not Found")
+            ),
+            "curl", "-X", "GET", "http://simple-artipie:8080/prometheus/metrics"
+        );
+    }
+
 }
