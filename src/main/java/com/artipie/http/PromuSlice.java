3b2bf1311568262f399146afcc143b9787291cc0
Olivier B. OURA <baudoliver7@gmail.com>
2022-02-04 12:52:37 +0000

    fix(pdd-action): bump v1.0.0 to master Ref: #889

--- src/main/java/com/artipie/http/PromuSlice.java
+++ src/main/java/com/artipie/http/PromuSlice.java
@@ -20,7 +20,6 @@ import java.io.StringWriter;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import org.reactivestreams.Publisher;
 
