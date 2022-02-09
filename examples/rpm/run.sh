0f4f96a39b80b44b2490d7bd09c0442d2cc5a0d0
olenagerasimova <olena.gereasimova@gmail.com>
2022-02-09 14:05:53 +0300

    fix: correct scripts

--- examples/rpm/run.sh
+++ examples/rpm/run.sh
@@ -3,6 +3,5 @@
 set -x
 set -e
 
-dnf config-manager --add-repo http://artipie.artipie:8080/my-rpm/
-dnf --skip-broken --disablerepo='*' --enablerepo='artipie.artipie_8080_my-rpm_' install wget
+dnf -y repository-packages example install wget
 
