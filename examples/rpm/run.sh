4d4ba43154dcd9fbeb0e9b5496ff0ca12287e9db
olenagerasimova <olena.gereasimova@gmail.com>
2022-02-09 14:40:16 +0300

    fix: add upload

--- examples/rpm/run.sh
+++ examples/rpm/run.sh
@@ -3,5 +3,6 @@
 set -x
 set -e
 
-dnf --skip-broken -y repository-packages example install time
+curl http://artipie.artipie:8080/my-rpm/time-1.7-45.el7.x86_64.rpm --upload-file "@time-1.7-45.el7.x86_64.rpm"
+dnf -y repository-packages example install
 
