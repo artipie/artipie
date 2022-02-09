8134e87b70bd0582e8e58948ae361800f23a1a32
olenagerasimova <olena.gereasimova@gmail.com>
2022-02-09 15:07:29 +0300

    correct script

--- examples/rpm/run.sh
+++ examples/rpm/run.sh
@@ -3,6 +3,6 @@
 set -x
 set -e
 
-curl http://artipie.artipie:8080/my-rpm/time-1.7-45.el7.x86_64.rpm --upload-file "@time-1.7-45.el7.x86_64.rpm"
+curl http://artipie.artipie:8080/my-rpm/time-1.7-45.el7.x86_64.rpm --upload-file "./time-1.7-45.el7.x86_64.rpm"
 dnf -y repository-packages example install
 
