c3d8d48a397d93ebf4af3c597f00f62db819d85e
olenagerasimova <olena.gereasimova@gmail.com>
2022-02-09 17:34:34 +0300

    try cat

--- examples/rpm/run.sh
+++ examples/rpm/run.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-cat /etc/yum.repos.d/example.repo
 curl -i -X PUT --data-binary "@time-1.7-45.el7.x86_64.rpm" http://artipie.artipie:8080/my-rpm/time-1.7-45.el7.x86_64.rpm
+cat /etc/yum.repos.d/example.repo
 dnf -y repository-packages example install
 
