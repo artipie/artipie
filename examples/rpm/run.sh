81ed4388ebd77ae4ca7bcd6614066dd1a6771329
olenagerasimova <olena.gereasimova@gmail.com>
2022-02-09 17:02:08 +0300

    check settings

--- examples/rpm/run.sh
+++ examples/rpm/run.sh
@@ -3,6 +3,7 @@
 set -x
 set -e
 
+less /etc/yum.repos.d/example.repo
 curl -i -X PUT --data-binary "@time-1.7-45.el7.x86_64.rpm" http://artipie.artipie:8080/my-rpm/time-1.7-45.el7.x86_64.rpm
 dnf -y repository-packages example install
 
