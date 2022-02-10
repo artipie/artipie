f0afef64043dd40400d7d0f3a55618e7ecaf7293
olenagerasimova <olena.gereasimova@gmail.com>
2022-02-10 08:47:39 +0300

    try copy settings

--- examples/rpm/run.sh
+++ examples/rpm/run.sh
@@ -4,6 +4,5 @@ set -x
 set -e
 
 curl -i -X PUT --data-binary "@time-1.7-45.el7.x86_64.rpm" http://artipie.artipie:8080/my-rpm/time-1.7-45.el7.x86_64.rpm
-curl -i -X GET http://artipie.artipie:8080/my-rpm/repodata/repomd.xml
 dnf -y repository-packages example install
 
