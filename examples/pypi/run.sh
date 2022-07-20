1309dcfe6e02e5d6b5cf3178848c8bcfda7394af
olenagerasimova <olena.gereasimova@gmail.com>
2022-07-20 16:08:41 +0300

    fix: correct trusted host

--- examples/pypi/run.sh
+++ examples/pypi/run.sh
@@ -12,5 +12,5 @@ python3 -m twine upload --repository-url http://artipie.artipie:8080/my-pypi \
 cd ..
 
 # Install earlier uploaded python package from artipie.
-python3 -m pip install --trusted-host artipie \
+python3 -m pip install --trusted-host artipie.artipie \
   --index-url http://artipie.artipie:8080/my-pypi sample_project
