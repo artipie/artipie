3a120f44d6d173e8a4c5014b36480bb666231104
Andrey Popov <andpopov@gmail.com>
2022-07-22 14:24:51 +0300

    fix: extended RpmITCase to test individual repository port

--- src/test/java/com/artipie/rpm/RpmITCase.java
+++ src/test/java/com/artipie/rpm/RpmITCase.java
@@ -11,19 +11,16 @@ import org.cactoos.list.ListOf;
 import org.hamcrest.core.IsEqual;
 import org.hamcrest.text.StringContainsInOrder;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.testcontainers.containers.BindMode;
 
 /**
  * IT case for RPM repository.
  * @since 0.12
- * @todo #1041:30min RpmITCase: Add test cases with repository on individual port: create one more
- *  repository with `port` settings and start it in Artipie container exposing the port with
- *  `withExposedPorts` method. Then, parameterize test cases to check repositories with different
- *  ports. Check `FileITCase` as an example.
  */
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 @DisabledOnOs(OS.WINDOWS)
@@ -32,11 +29,14 @@ public final class RpmITCase {
     /**
      * Test deployments.
      * @checkstyle VisibilityModifierCheck (10 lines)
+     * @checkstyle MagicNumberCheck (10 lines)
      */
     @RegisterExtension
     final TestDeployment containers = new TestDeployment(
         () -> TestDeployment.ArtipieContainer.defaultDefinition()
-            .withRepoConfig("rpm/my-rpm.yml", "my-rpm"),
+            .withRepoConfig("rpm/my-rpm.yml", "my-rpm")
+            .withRepoConfig("rpm/my-rpm-port.yml", "my-rpm-port")
+            .withExposedPorts(8081),
         () -> new TestDeployment.ClientContainer("fedora:35")
             .withClasspathResourceMapping(
                 "rpm/time-1.7-45.el7.x86_64.rpm", "/w/time-1.7-45.el7.x86_64.rpm",
@@ -49,24 +49,29 @@ public final class RpmITCase {
         this.containers.assertExec(
             "Dnf install curl failed", new ContainerResultMatcher(), "dnf", "-y", "install", "curl"
         );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "8080,my-rpm",
+        "8081,my-rpm-port"
+    })
+    void uploadsAndInstallsThePackage(final String port, final String repo) throws Exception {
         this.containers.putBinaryToClient(
             String.join(
                 "\n", "[example]",
                 "name=Example Repository",
-                "baseurl=http://artipie:8080/my-rpm",
+                String.format("baseurl=http://artipie:%s/%s", port, repo),
                 "enabled=1",
                 "gpgcheck=0"
             ).getBytes(),
             "/etc/yum.repos.d/example.repo"
         );
-    }
-
-    @Test
-    void uploadsAndInstallsThePackage() throws Exception {
         this.containers.assertExec(
             "Failed to upload rpm package",
             new ContainerResultMatcher(),
-            "curl", "http://artipie:8080/my-rpm/time-1.7-45.el7.x86_64.rpm",
+            "curl",
+            String.format("http://artipie:%s/%s/time-1.7-45.el7.x86_64.rpm", port, repo),
             "--upload-file", "/w/time-1.7-45.el7.x86_64.rpm"
         );
         // @checkstyle MagicNumberCheck (1 line)
