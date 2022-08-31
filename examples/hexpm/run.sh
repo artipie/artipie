d6a805366271b33d7089db8b73f55a2f4971f618
Aleksei Borodin <swizbizy@gmail.com>
2022-08-31 13:41:12 +0300

    fix: change uri path in hexpm smoke test

--- examples/hexpm/run.sh
+++ examples/hexpm/run.sh
@@ -3,14 +3,15 @@ set -x
 set -e
 
 # Upload tar via curl.
-curl -X POST --data-binary "@sample-for-deployment/decimal-2.0.0.tar" http://artipie.artipie:8080/hexpm/publish?replace=false
+cd sample-for-deployment
+curl -X POST --data-binary "@decimal-2.0.0.tar" http://artipie.artipie:8080/my-hexpm/publish?replace=false
 
 # Install mix
-cd sample-consumer/kv
+cd ../sample-consumer/kv
 mix local.hex --force
 
 # Add ref to Artipie repository.
-mix hex.repo add my_repo http://artipie.artipie:8080/hexpm
+mix hex.repo add my_repo http://artipie.artipie:8080/my-hexpm
 
 # Fetch the uploaded tar.
 mix hex.package fetch decimal 2.0.0 --repo=my_repo
