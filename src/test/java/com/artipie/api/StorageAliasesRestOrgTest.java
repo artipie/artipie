ce03d354bf670c61f9758bba653e633a6b4cf559
olenagerasimova <olena.gereasimova@gmail.com>
2022-09-07 12:06:18 +0300

    feat: add/update aliases

--- src/test/java/com/artipie/api/StorageAliasesRestOrgTest.java
+++ src/test/java/com/artipie/api/StorageAliasesRestOrgTest.java
@@ -8,8 +8,13 @@ import com.artipie.asto.Key;
 import com.artipie.asto.misc.UncheckedConsumer;
 import com.artipie.settings.StorageAliases;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxTestContext;
 import java.nio.charset.StandardCharsets;
+import org.eclipse.jetty.http.HttpStatus;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
@@ -17,6 +22,7 @@ import org.skyscreamer.jsonassert.JSONAssert;
  * Test for {@link StorageAliasesRest} with org layout.
  * @since 0.27
  */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public final class StorageAliasesRestOrgTest extends RestApiServerBase {
 
     @Test
@@ -73,6 +79,117 @@ public final class StorageAliasesRestOrgTest extends RestApiServerBase {
                     true
                 )
             )
+        );
+    }
+
+    @Test
+    void addsNewUserAlias(final Vertx vertx, final VertxTestContext ctx) throws Exception {
+        final String uname = "john";
+        this.requestAndAssert(
+            vertx, ctx,
+            new TestRequest(
+                HttpMethod.PUT, String.format("/api/v1/storages/%s/new-alias", uname),
+                new JsonObject().put("type", "file").put("path", "john/new/alias/path")
+            ),
+            resp -> {
+                MatcherAssert.assertThat(
+                    resp.statusCode(), new IsEqual<>(HttpStatus.CREATED_201)
+                );
+                MatcherAssert.assertThat(
+                    new String(
+                        this.storage().value(new Key.From(uname, StorageAliases.FILE_NAME)),
+                        StandardCharsets.UTF_8
+                    ),
+                    new IsEqual<>(
+                        String.join(
+                            "\n",
+                            "storages:",
+                            "  \"new-alias\":",
+                            "    type: file",
+                            "    path: john/new/alias/path"
+                        )
+                    )
+                );
+            }
+        );
+    }
+
+    @Test
+    void updatesRepoAlias(final Vertx vertx, final VertxTestContext ctx) throws Exception {
+        final String rname = "my-pypi";
+        final String uname = "mark";
+        final Key.From key = new Key.From(uname, rname, StorageAliases.FILE_NAME);
+        this.save(key, this.yamlAliases().getBytes(StandardCharsets.UTF_8));
+        this.requestAndAssert(
+            vertx, ctx, new TestRequest(
+                HttpMethod.PUT,
+                String.format("/api/v1/repository/%s/%s/storages/local", uname, rname),
+                new JsonObject().put("type", "file").put("path", "/var/artipie/python/local")
+            ),
+            resp -> {
+                MatcherAssert.assertThat(
+                    resp.statusCode(), new IsEqual<>(HttpStatus.CREATED_201)
+                );
+                MatcherAssert.assertThat(
+                    new String(this.storage().value(key), StandardCharsets.UTF_8),
+                    new IsEqual<>(
+                        String.join(
+                            "\n",
+                            "storages:",
+                            "  local:",
+                            "    type: file",
+                            "    path: /var/artipie/python/local",
+                            "  \"s3-sto\":",
+                            "    type: s3",
+                            "    bucket: any",
+                            "    region: east",
+                            "    endpoint: \"https://minio.selfhosted/s3\""
+                        )
+                    )
+                );
+            }
+        );
+    }
+
+    @Test
+    void addsNewCommonAlias(final Vertx vertx, final VertxTestContext ctx) throws Exception {
+        this.save(
+            new Key.From(StorageAliases.FILE_NAME),
+            this.yamlAliases().getBytes(StandardCharsets.UTF_8)
+        );
+        this.requestAndAssert(
+            vertx, ctx, new TestRequest(
+                HttpMethod.PUT, "/api/v1/storages/new-alias",
+                new JsonObject().put("type", "file").put("path", "new/alias/path")
+            ),
+            resp -> {
+                MatcherAssert.assertThat(
+                    resp.statusCode(), new IsEqual<>(HttpStatus.CREATED_201)
+                );
+                MatcherAssert.assertThat(
+                    new String(
+                        this.storage().value(new Key.From(StorageAliases.FILE_NAME)),
+                        StandardCharsets.UTF_8
+                    ),
+                    new IsEqual<>(
+                        String.join(
+                            "\n",
+                            "storages:",
+                            "  local:",
+                            "    type: fs",
+                            "    path: /var/artipie/local/data",
+                            "  \"s3-sto\":",
+                            "    type: s3",
+                            "    bucket: any",
+                            "    region: east",
+                            "    endpoint: \"https://minio.selfhosted/s3\"",
+                            "  \"new-alias\":",
+                            "    type: file",
+                            "    path: new/alias/path"
+                        )
+                    )
+                );
+            }
         );
     }
 
