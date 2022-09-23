4295de09d5ad29759a603530c3b4083c141949da
Alena <olena.gerasimova@gmail.com>
2022-09-23 16:24:58 +0300

    feat: added more tests for authentication (#1149)
    Co-authored-by: olenagerasimova <olena.gereasimova@gmail.com>

--- src/test/java/com/artipie/api/AuthRestFlatTest.java
+++ src/test/java/com/artipie/api/AuthRestFlatTest.java
@@ -7,7 +7,6 @@ package com.artipie.api;
 import com.artipie.http.auth.Authentication;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpMethod;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxTestContext;
 import java.util.Collection;
@@ -21,59 +20,53 @@ import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 
 /**
- * Test for authentication in Rest API for org layout.
+ * Test for authentication in Rest API for flat layout.
+ * As users management API is the same for org and flat, layout,
+ * it's tested only in {@link AuthRestOrgTest}.
  * @since 0.27
  */
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
-public final class AuthRestOrgTest extends RestApiServerBase {
+public final class AuthRestFlatTest extends RestApiServerBase {
 
     /**
      * Name of the user.
      */
-    private static final String NAME = "Aladdin";
+    private static final String NAME = "Max";
 
     /**
      * User password.
      */
-    private static final String PASS = "opensesame";
+    private static final String PASS = "madmax";
 
     /**
      * List of the GET requests, which do not require existence of any settings.
      */
     private static final Collection<TestRequest> GET_DATA = List.of(
-        new TestRequest(HttpMethod.GET, "/api/v1/users"),
         new TestRequest(HttpMethod.GET, "/api/v1/repository/list"),
-        new TestRequest(HttpMethod.GET, "/api/v1/repository/Cate/my-npm/storages"),
-        new TestRequest(HttpMethod.GET, "/api/v1/storages/Oleg"),
+        new TestRequest(HttpMethod.GET, "/api/v1/repository/my-npm/storages"),
         new TestRequest(HttpMethod.GET, "/api/v1/storages")
     );
 
     /**
-     * List of the existing requests for org layout.
+     * List of the existing requests for flat layout.
      */
     private static final Collection<TestRequest> RQST = Stream.concat(
         Stream.of(
-            new TestRequest(HttpMethod.PUT, "/api/v1/users/Mark"),
-            new TestRequest(HttpMethod.GET, "/api/v1/users/Alice"),
-            new TestRequest(HttpMethod.DELETE, "/api/v1/users/Justine"),
-            new TestRequest(HttpMethod.GET, "/api/v1/repository/list/John"),
-            new TestRequest(HttpMethod.GET, "/api/v1/repository/Olga/my-maven"),
-            new TestRequest(HttpMethod.PUT, "/api/v1/repository/Jane/rpm"),
-            new TestRequest(HttpMethod.DELETE, "/api/v1/repository/Sasha/my-python"),
-            new TestRequest(HttpMethod.PUT, "/api/v1/repository/Alex/bin-files/move"),
-            new TestRequest(HttpMethod.PUT, "/api/v1/repository/Katie/my-go/storages/local"),
-            new TestRequest(HttpMethod.DELETE, "/api/v1/repository/Ann/docker/storages/s3sto"),
+            new TestRequest(HttpMethod.GET, "/api/v1/repository/my-maven"),
+            new TestRequest(HttpMethod.PUT, "/api/v1/repository/rpm"),
+            new TestRequest(HttpMethod.DELETE, "/api/v1/repository/my-python"),
+            new TestRequest(HttpMethod.PUT, "/api/v1/repository/bin-files/move"),
+            new TestRequest(HttpMethod.PUT, "/api/v1/repository/my-go/storages/local"),
+            new TestRequest(HttpMethod.DELETE, "/api/v1/repository/docker/storages/s3sto"),
             new TestRequest(HttpMethod.PUT, "/api/v1/storages/def"),
-            new TestRequest(HttpMethod.DELETE, "/api/v1/storages/local-dir"),
-            new TestRequest(HttpMethod.PUT, "/api/v1/storages/Andrew/s3-shared"),
-            new TestRequest(HttpMethod.DELETE, "/api/v1/storages/Dmitrii/s3-amazon")
-        ), AuthRestOrgTest.GET_DATA.stream()
+            new TestRequest(HttpMethod.DELETE, "/api/v1/storages/local-dir")
+        ), AuthRestFlatTest.GET_DATA.stream()
     ).toList();
 
     @Test
     void returnsUnauthorizedWhenTokenIsAbsent(final Vertx vertx, final VertxTestContext ctx)
         throws Exception {
-        for (final TestRequest item : AuthRestOrgTest.RQST) {
+        for (final TestRequest item : AuthRestFlatTest.RQST) {
             this.requestAndAssert(
                 vertx, ctx, item, Optional.empty(),
                 response -> MatcherAssert.assertThat(
@@ -88,9 +81,10 @@ public final class AuthRestOrgTest extends RestApiServerBase {
     @Test
     void returnsOkWhenTokenIsPresent(final Vertx vertx, final VertxTestContext ctx)
         throws Exception {
-        final AtomicReference<String> token =
-            this.getToken(vertx, ctx, AuthRestOrgTest.NAME, AuthRestOrgTest.PASS);
-        for (final TestRequest item : AuthRestOrgTest.GET_DATA) {
+        final AtomicReference<String> token = this.getToken(
+            vertx, ctx, AuthRestFlatTest.NAME, AuthRestFlatTest.PASS
+        );
+        for (final TestRequest item : AuthRestFlatTest.GET_DATA) {
             this.requestAndAssert(
                 vertx, ctx, item, Optional.of(token.get()),
                 response -> MatcherAssert.assertThat(
@@ -106,8 +100,8 @@ public final class AuthRestOrgTest extends RestApiServerBase {
     void createsAndRemovesRepoWithAuth(final Vertx vertx, final VertxTestContext ctx)
         throws Exception {
         final AtomicReference<String> token =
-            this.getToken(vertx, ctx, AuthRestOrgTest.NAME, AuthRestOrgTest.PASS);
-        final String path = "/api/v1/repository/john/my-docker";
+            this.getToken(vertx, ctx, AuthRestFlatTest.NAME, AuthRestFlatTest.PASS);
+        final String path = "/api/v1/repository/my-docker";
         this.requestAndAssert(
             vertx, ctx,
             new TestRequest(
@@ -131,42 +125,16 @@ public final class AuthRestOrgTest extends RestApiServerBase {
     }
 
     @Test
-    void createsAndRemovesUserWithAuth(final Vertx vertx, final VertxTestContext ctx)
-        throws Exception {
-        final AtomicReference<String> token =
-            this.getToken(vertx, ctx, AuthRestOrgTest.NAME, AuthRestOrgTest.PASS);
-        final String path = "/api/v1/users/Alice";
-        this.requestAndAssert(
-            vertx, ctx, new TestRequest(
-                HttpMethod.PUT, path,
-                new JsonObject().put("type", "plain").put("pass", "wonderland")
-                    .put("groups", JsonArray.of("readers", "tags"))
-            ), Optional.of(token.get()),
-            response -> MatcherAssert.assertThat(
-                response.statusCode(),
-                new IsEqual<>(HttpStatus.CREATED_201)
-            )
-        );
-        this.requestAndAssert(
-            vertx, ctx, new TestRequest(HttpMethod.DELETE, path), Optional.of(token.get()),
-            response -> MatcherAssert.assertThat(
-                response.statusCode(),
-                new IsEqual<>(HttpStatus.OK_200)
-            )
-        );
-    }
-
-    @Test
     void createsAndRemovesStorageAliasWithAuth(final Vertx vertx, final VertxTestContext ctx)
         throws Exception {
         final AtomicReference<String> token =
-            this.getToken(vertx, ctx, AuthRestOrgTest.NAME, AuthRestOrgTest.PASS);
-        final String path = "/api/v1/storages/Jane/new-alias";
+            this.getToken(vertx, ctx, AuthRestFlatTest.NAME, AuthRestFlatTest.PASS);
+        final String path = "/api/v1/storages/new-alias";
         this.requestAndAssert(
             vertx, ctx,
             new TestRequest(
                 HttpMethod.PUT, path,
-                new JsonObject().put("type", "file").put("path", "jane/new/alias/path")
+                new JsonObject().put("type", "file").put("path", "/new/alias/path")
             ),
             resp -> MatcherAssert.assertThat(
                 resp.statusCode(),
@@ -184,11 +152,11 @@ public final class AuthRestOrgTest extends RestApiServerBase {
 
     @Override
     Authentication auth() {
-        return new Authentication.Single(AuthRestOrgTest.NAME, AuthRestOrgTest.PASS);
+        return new Authentication.Single(AuthRestFlatTest.NAME, AuthRestFlatTest.PASS);
     }
 
     @Override
     String layout() {
-        return "org";
+        return "flat";
     }
 }
