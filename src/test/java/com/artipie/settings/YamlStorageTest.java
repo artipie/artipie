d09ecfd75c56a61e64554c186e5a510a96f8b0f2
olenagerasimova <olena.gereasimova@gmail.com>
2022-10-18 16:33:22 +0300

    removed tests with s3 storage

--- src/test/java/com/artipie/settings/YamlStorageTest.java
+++ src/test/java/com/artipie/settings/YamlStorageTest.java
@@ -6,7 +6,6 @@ package com.artipie.settings;
 
 import com.amihaiemil.eoyaml.Yaml;
 import com.artipie.asto.fs.FileStorage;
-import com.artipie.asto.s3.S3Storage;
 import java.util.stream.Stream;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -32,51 +31,6 @@ class YamlStorageTest {
         MatcherAssert.assertThat(
             settings.storage(),
             Matchers.instanceOf(FileStorage.class)
-        );
-    }
-
-    @Test
-    public void shouldBuildS3StorageFromFullSettings() throws Exception {
-        final YamlStorage settings = new YamlStorage(
-            Yaml.createYamlInput(
-                String.join(
-                    "",
-                    "type: s3\n",
-                    "bucket: my-bucket\n",
-                    "region: my-region\n",
-                    "endpoint: https://my-s3-provider.com\n",
-                    "credentials:\n",
-                    "  type: basic\n",
-                    "  accessKeyId: ***\n",
-                    "  secretAccessKey: ***\n"
-                )
-            ).readYamlMapping()
-        );
-        MatcherAssert.assertThat(
-            settings.storage(),
-            Matchers.instanceOf(S3Storage.class)
-        );
-    }
-
-    @Test
-    public void shouldBuildS3StorageFromMinimalSettings() throws Exception {
-        System.getProperties().put("aws.region", "my-region");
-        final YamlStorage settings = new YamlStorage(
-            Yaml.createYamlInput(
-                String.join(
-                    "",
-                    "type: s3\n",
-                    "bucket: my-bucket\n",
-                    "credentials:\n",
-                    "  type: basic\n",
-                    "  accessKeyId: ***\n",
-                    "  secretAccessKey: ***\n"
-                )
-            ).readYamlMapping()
-        );
-        MatcherAssert.assertThat(
-            settings.storage(),
-            Matchers.instanceOf(S3Storage.class)
         );
     }
 
