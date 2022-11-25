e5dfc0a49d874fe7580b6b6692ca53798e501774
Denis Garus <garus.d.g@gmail.com>
2022-11-25 09:08:45 +0300

    `Content` wrapper that allows to get chunks count and received data size (#1186)
    * `Content` wrapper that allows to get chunks count and received data size
    
    * Fix code style

---
+++ src/main/java/com/artipie/jfr/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/artipie/LICENSE.txt
+ */
+
+/**
+ * Artipie files related to JFR.
+ *
+ * @since 0.28.0
+ */
+package com.artipie.jfr;
