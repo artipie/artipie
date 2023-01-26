770ae6dbd092b33b2ede419934294a6306af000c
Andrey Popov <andpopov@gmail.com>
2023-01-26 11:39:54 +0300

    Keycloak authentication

---
+++ src/test/java/com/artipie/tools/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/artipie/LICENSE.txt
+ */
+
+/**
+ * Artipie tools for dynamic compiling and loading compiled classes.
+ *
+ * @since 0.28
+ */
+package com.artipie.tools;
