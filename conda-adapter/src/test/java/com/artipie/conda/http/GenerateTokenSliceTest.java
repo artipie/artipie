960df61a3d88fdff15554ea36359708ca8470704
dgarus <garus.d.g@gmail.com>
2023-12-02 21:14:04 +0300

    fix failed test

--- conda-adapter/src/test/java/com/artipie/conda/http/GenerateTokenSliceTest.java
+++ conda-adapter/src/test/java/com/artipie/conda/http/GenerateTokenSliceTest.java
@@ -35,6 +35,11 @@ class GenerateTokenSliceTest {
      */
     private static final String TOKEN = "abc123";
 
+    /**
+     * Anonymous token.
+     */
+    private static final String ANONYMOUS_TOKEN = "anonymous123";
+
     @Test
     void addsToken() {
         final String name = "Alice";
@@ -63,12 +68,35 @@ class GenerateTokenSliceTest {
     void returnsUnauthorized() {
         MatcherAssert.assertThat(
             new GenerateTokenSlice(
-                new Authentication.Single("Any", "123"),
+                new Authentication.Single("Jora", "123"),
                 new FakeAuthTokens()
             ),
             new SliceHasResponse(
                 new RsHasStatus(RsStatus.UNAUTHORIZED),
-                new RequestLine(RqMethod.POST, "/any/line")
+                new RequestLine(RqMethod.POST, "/any/line"),
+                new Headers.From(new Authorization.Basic("Jora", "0987")),
+                Content.EMPTY
+            )
+        );
+    }
+
+    @Test
+    void anonymousToken() {
+        MatcherAssert.assertThat(
+            "Slice response in not 200 OK",
+            new GenerateTokenSlice(
+                new Authentication.Single("test_user", "aaa"),
+                new FakeAuthTokens()
+            ),
+            new SliceHasResponse(
+                Matchers.allOf(
+                    new RsHasStatus(RsStatus.OK),
+                    new RsHasBody(
+                        String.format("{\"token\":\"%s\"}", GenerateTokenSliceTest.ANONYMOUS_TOKEN)
+                            .getBytes()
+                    )
+                ),
+                new RequestLine(RqMethod.POST, "/authentications")
             )
         );
     }
@@ -86,7 +114,9 @@ class GenerateTokenSliceTest {
 
         @Override
         public String generate(final AuthUser user) {
-            return GenerateTokenSliceTest.TOKEN;
+            return user.isAnonymous()
+                ? GenerateTokenSliceTest.ANONYMOUS_TOKEN
+                : GenerateTokenSliceTest.TOKEN;
         }
     }
 
