dcbcd6357e6541d073ce6a40e1b09d3eef70d748
dgarus <garus.d.g@gmail.com>
2024-03-19 13:50:26 +0300

    Slice.send takes Content

--- gem-adapter/src/test/java/com/artipie/gem/YamlMetaFormatTest.java
+++ gem-adapter/src/test/java/com/artipie/gem/YamlMetaFormatTest.java
@@ -9,10 +9,13 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
 import org.hamcrest.text.StringContainsInOrder;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 /**
  * Test case for {@link YamlMetaFormat}.
  */
+@DisabledOnOs(OS.WINDOWS)
 final class YamlMetaFormatTest {
     @Test
     void addPlainString() {
