ba176cdedbb848aec10d6d9ed5baa70d0bc4d2b6
dgarus <garus.d.g@gmail.com>
2024-03-22 17:17:41 +0300

    Response status refactoring

--- debian-adapter/src/test/java/com/artipie/debian/DebianSliceS3ITCase.java
+++ debian-adapter/src/test/java/com/artipie/debian/DebianSliceS3ITCase.java
@@ -220,7 +220,7 @@ public final class DebianSliceS3ITCase {
         MatcherAssert.assertThat(
             "Response for upload is OK",
             con.getResponseCode(),
-            new IsEqual<>(Integer.parseInt(RsStatus.OK.code()))
+            new IsEqual<>(RsStatus.OK.code())
         );
         MatcherAssert.assertThat("Event was added to queue", this.events.size() == 1);
         this.exec("apt-get", "update");
